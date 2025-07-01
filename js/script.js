1bec534284f2f83e66d46c21c75e427004f40d16
dlowes1 <dan141@gmail.com>
2025-07-01 21:59:54 +0100

    3rd Attempt at js changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=51.5073219&lon=0.1276474&dt={time}&appid=f23ee9deb4e1a7450f3157c44ed020e1
 const API_KEY = "f23ee9deb4e1a7450f3157c44ed020e1";
-
+//adssa
 // First, get the latitude and longitude for the city
 const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
 // https://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=f23ee9deb4e1a7450f3157c44ed020e1
